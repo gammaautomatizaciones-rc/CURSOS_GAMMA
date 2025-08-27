d4e5fdc741deb3e90bd64cbf19fe2f241cada602
gammaautomatizaciones-rc <gamma.automatizaciones@gmail.com>
2025-08-27 11:11:50 -0300

    Update excel.js

--- cursos/excel/excel.js
+++ cursos/excel/excel.js
@@ -8,7 +8,7 @@ if (!usuarioExcel) {
 
 // =============================
 // Barra de progreso
-// =============================
+// ===========================
 function actualizarProgreso(habilitados, total = 12) {
   const porcentaje = total > 0 ? Math.round((habilitados / total) * 100) : 0;
   document.getElementById("barra-progreso").style.width = `${porcentaje}%`;
